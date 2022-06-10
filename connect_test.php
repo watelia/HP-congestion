15db2acdcb3d1949c6a1ecbdd425031165bb37bb
Romeo Tello <106936209+watelia@users.noreply.github.com>
2022-06-10 16:08:57 +0900

    使わないコードを消去、jsonの読み込み方法を変更

--- connect_test.php
+++ connect_test.php
@@ -7,30 +7,19 @@ $array = json_decode($json, true);
 
 
 // 入力するダミーデータを設定
-// idはオートインクリメントなので何も入力しない
 $state = 4;
 
 
 // 追加するクエリ
-$sqlq1 = "INSERT INTO congestion (state, time) VALUES (:stm, now())";
+$sqlq = "INSERT INTO congestion (state, time) VALUES (:stm, now())";
 
 
 // PDOの設定
-$dsn = $array["dsn"][0];
-$db_user = $array["db_user"][0];
-$db_pass = $array["db_pass"][0];
+$dsn = $array["dsn"];
+$db_user = $array["db_user"];
+$db_pass = $array["db_pass"];
 $pdo = new PDO($dsn, $db_user, $db_pass);
-$stmt = $pdo->prepare($sqlq1);
+$stmt = $pdo->prepare($sqlq);
 
 // 実行
 $stmt->execute(array(':stm' => $state));
-
-
-// $sqlq2 = "SELECT * FROM congestion ORDER BY id DESC LIMIT " . $number . ";";
-
-// $stmt = $pdo->prepare($sqlq2);
-
-// $stmt->execute();
-
-// $result = $stmt->fetch(PDO::FETCH_ASSOC);
-// var_dump($number);
