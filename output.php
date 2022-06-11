296fa2d91f51b7e96fad6d13edffd2e4f9bf8c81
Romeo Tello <106936209+watelia@users.noreply.github.com>
2022-06-11 14:22:52 +0900

    ifでの表示内容の分岐をswitchに変更

--- output.php
+++ output.php
@@ -42,8 +42,6 @@ $situation = "";
 ?>
 
 
-
-
 <!doctype html>
 <html lang="ja">
 
@@ -60,7 +58,6 @@ $situation = "";
 
 <body>
     <h1>X▶CUBE　ウェブページ混雑表示状況表示</h1>
-
 
     <main>
         <?php if ($timediff <= 7200) {
@@ -99,8 +96,6 @@ $situation = "";
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-
 
 
 </body>
